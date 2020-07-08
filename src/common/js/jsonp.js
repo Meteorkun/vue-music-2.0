281a5720fadf14fc079653b7f6cfb71195825d1a
Meteorkun <33950309+Meteorkun@users.noreply.github.com>
2020-07-08 09:51:13 +0800

    获取首页轮播图数据

--- src/common/js/jsonp.js
+++ src/common/js/jsonp.js
@@ -1,18 +1,18 @@
 import originJsonp from 'jsonp'
 
-export default function jsonp(url,data,option){
-  //如果没有？加一个？,否则加 &
+export default function jsonp(url, data, option) {
+   //如果没有？加一个？,否则加 &
   url += (url.indexOf('?') < 0 ? '?' : '&') + param(data)
 
-  return new Promise((resolve,reject) => {
-    originJsonp(url,option,(err,data) => {
-      if(!err){
+  return new Promise((resolve, reject) => {
+    originJsonp(url, option, (err, data) => {
+      if (!err) {
         resolve(data)
-      }else{
+      } else {
         reject(err)
       }
     })
-  }
+  })
 }
 
 export function param(data){
