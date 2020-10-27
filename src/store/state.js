83ffb2bee7b817e7f2efe4da3974e176c1024286
Meteorkun <33950309+Meteorkun@users.noreply.github.com>
2020-10-27 15:43:54 +0800

    播放器Vuex数据设计

--- src/store/state.js
+++ src/store/state.js
@@ -1,5 +1,19 @@
+import {playMode} from 'common/js/config'
+
 const state = {
-  singer: {}
+  singer: {},
+  // 歌曲是否播放
+  playing: false,
+  // 歌曲是否全屏播放
+  fullScreen: false,
+  // 歌曲播放列表
+  playlist: [],
+  // 歌曲顺序播放列表
+  sequenceList: [],
+  // 歌曲播放模式
+  mode: playMode.sequence,
+  // 当前歌曲播放索引
+  currentIndex: -1
 }
 
 export default state
