09c2cb4109f37f86b7008a2b781997bde30b3a9e
Meteorkun <33950309+Meteorkun@users.noreply.github.com>
2020-11-04 09:37:01 +0800

    歌单详情页数据的处理和应用

--- src/api/recommend.js
+++ src/api/recommend.js
@@ -103,10 +103,60 @@ export function getPlaySongVkey(songmid) {
   })
 }
 
+// 获取推荐歌单数据
+export function getSongList(disstid) {
+  const url = 'https://c.y.qq.com/qzone/fcg-bin/fcg_ucc_getcdinfo_byids_cp.fcg'
+  const data = Object.assign({}, commonParams, {
+    disstid,
+    type: 1,
+    json: 1,
+    utf8: 1,
+    onlysong: 0,
+    // new_format: 1,
+    platform: 'yqq',
+    hostUin: 0,
+    needNewCode: 0,
+    g_tk: 1928093487
+  })
+  return jsonp(url, data, options)
+}
+
+// export function getSongList(disstid) {
+//   const url = '/api/getSongList'
+//
+//   const data = Object.assign({}, commonParams, {
+//     uin: 0,
+//     format: 'json',
+//     notice: 0,
+//     needNewCode: 1,
+//     new_format: 1,
+//     pic: 500,
+//     // 关键数据
+//     disstid,
+//     type: 1,
+//     json: 1,
+//     utf8: 1,
+//     onlysong: 0,
+//     picmid: 1,
+//     nosign: 1,
+//     song_begin: 0,
+//     platform: 'yqq',
+//     song_num: 100,
+//     _: +new Date()
+//   })
+//
+//   return axios.get(url, {
+//     params: data
+//   }).then((res) => {
+//     return Promise.resolve(res.data)
+//   })
+// }
+
 // export function getSongList(disstid) {
 //   const url = 'https://c.y.qq.com/qzone/fcg-bin/fcg_ucc_getcdinfo_byids_cp.fcg'
-
+//
 //   const data = Object.assign({}, commonParams, {
+
 //     disstid,
 //     type: 1,
 //     json: 1,
--- src/components/disc/disc.vue
+++ src/components/disc/disc.vue
@@ -1,12 +1,15 @@
 <template>
   <transition name="slide">
-    <music-list :title="title" :bg-image="bgImage"></music-list>
+    <music-list :title="title" :bg-image="bgImage" :songs="songs"></music-list>
   </transition>
 </template>
 
 <script type="text/ecmascript-6">
   import MusicList from 'components/music-list/music-list'
   import {mapGetters} from 'vuex'
+  import {getSongList, getPlaySongVkey} from 'api/recommend'
+  import {ERR_OK} from 'api/config'
+  import {createSong} from 'common/js/song'
 
   export default {
     computed: {
@@ -19,6 +22,43 @@
       ...mapGetters([
         'disc'
       ])
+    },
+    created() {
+      this._getSongList()
+    },
+    data() {
+      return {
+        songs: []
+      }
+    },
+    methods: {
+      _getSongList() {
+        if (!this.disc.dissid) { // 在歌单详情页强制刷新后，即没有获得id时，回退到推荐页面
+          this.$router.push('/recommend')
+          return
+        }
+        getSongList(this.disc.dissid).then((res) => {
+          if (res.code === ERR_OK) {
+            // console.log(res.cdlist[0].songlist)
+            this.songs = this._normalizeSongs(res.cdlist[0].songlist)
+          }
+        })
+      },
+      _normalizeSongs(list) {
+        let ret = []
+        list.forEach((musicData) => {
+          console.log(musicData)
+          if (musicData.songid && musicData.albumid) {
+            getPlaySongVkey(musicData.songmid).then((res) => {
+              if (res) {
+                ret.push(createSong(musicData, res))
+              }
+            })
+            // ret.push(createSong(musicData))
+          }
+        })
+        return ret
+      }
     },
     components: {
       MusicList
